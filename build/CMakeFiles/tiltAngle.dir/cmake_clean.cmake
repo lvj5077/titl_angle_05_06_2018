c3d1ae10b04a74624077603c9799007096a63da3
Lingqiu Jin <lingqiujin@Lingqius-MacBook-Pro.local>
2018-05-10 16:00:23 -0400

    include eigen

---
+++ build/CMakeFiles/tiltAngle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiltAngle.dir/tiltAngle.cpp.o"
+  "tiltAngle.pdb"
+  "tiltAngle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tiltAngle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- tiltAngle.cpp
+++ tiltAngle.cpp
@@ -1,18 +1,16 @@
 #include "imu_reader.h"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <iostream>
 
 #define D2R(d) (((d)/180.)*M_PI)
+#define R2D(r) ((r)*180./M_PI)
 
-void CMotorIntend::computeTilt()
+using namespace std;
+int main(int argc, char* argv[])
 {
-  class CIMUReader; 
 
-  CIMUReader * mpIMU; 
-
-  if(mpIMU == 0)
-  {
-    mpIMU = new CIMUReader; 
-  }
-
+  CIMUReader imu_reader; 
 
   double a_gy = 0; 
   double a_gz = 0; 
@@ -22,7 +20,9 @@ void CMotorIntend::computeTilt()
 
   for(int i=0; i< N; i++)
   {
-    IMUData d = mpIMU->getNewIMUData(); 
+    IMUData d = imu_reader.getNewIMUData(); 
+    printf(" %d data: %+#7.2f %+#7.2f %+#7.2f %+#7.2f %+#7.2f %+#7.2f\n", i, d.mroll, 
+          d.mpitch, d.myaw, d.max, d.may, d.maz);
     a_gy += d.may; 
     a_gz += d.maz;
   }
@@ -37,7 +37,7 @@ void CMotorIntend::computeTilt()
   double ca = cos(-(M_PI/2. - m_alpha));
   double sa = sin(-(M_PI/2. - m_alpha)); 
 
-  Ro2i = Eigen::Matrix3d::Identity(); // coordinate Origin (located on the cane) to IMU
+  Eigen::Matrix3d Ro2i = Eigen::Matrix3d::Identity(); // coordinate Origin (located on the cane) to IMU
   Ro2i(1,1) = ca; Ro2i(1,2) = -sa;
   Ro2i(2,1) = sa; Ro2i(2,2) = ca; 
   Eigen::Vector3d gI{0., a_gy, a_gz}; 
@@ -47,7 +47,7 @@ void CMotorIntend::computeTilt()
   double theta = atan2(gO(1), gO(2)); 
   cout <<"motor_intend: tilt angle theta = "<<R2D(fabs(theta))<<endl;
 
-  m_theta = theta > 0? theta : -theta; 
-  return ; 
+  double m_theta = theta > 0? theta : -theta; 
+  return 0; 
   
 }
